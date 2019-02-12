d9eba16855d863c0ca866606b6f5b0a9947890df
Mao <160663534@qq.com>
2019-02-12 18:41:58 +0800

    the second class

--- PPMToolFullStack/src/main/java/io/mann/ppmtool/domain/Project.java
+++ PPMToolFullStack/src/main/java/io/mann/ppmtool/domain/Project.java
@@ -1,8 +1,131 @@
 package io.mann.ppmtool.domain;
 
+import javax.persistence.*;
+import java.util.Date;
+
 /**
  * @Author: mann
  * @Date: 2019-02-09 19:45
  */
+@Entity
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String projectName;
+
+    /**
+     * 项目标识符
+     */
+    private String projectIdentifier;
+
+    /**
+     * 描述
+     */
+    private String  description;
+
+    /**
+     * 开始时间
+     */
+    private Date start_date;
+
+    /**
+     * 结束时间
+     */
+    private Date end_date;
+
+    /**
+     * 创建时间
+     */
+    private Date created_At;
+
+    /**
+     * 更新时间
+     */
+    private Date updated_At;
+
+    public Project() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(Date start_date) {
+        this.start_date = start_date;
+    }
+
+    public Date getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(Date end_date) {
+        this.end_date = end_date;
+    }
+
+    public Date getCreated_At() {
+        return created_At;
+    }
+
+    public void setCreated_At(Date created_At) {
+        this.created_At = created_At;
+    }
+
+    public Date getUpdated_At() {
+        return updated_At;
+    }
+
+    public void setUpdated_At(Date updated_At) {
+        this.updated_At = updated_At;
+    }
+
+    /**
+     * @PrePersist 记录与业务无关的字段。
+     */
+    @PrePersist
+    protected void onCreate() {
+        this.created_At = new Date();
+    }
+
+    /**
+     * @PreUpdate 用于为相应的生命周期事件指定回调方法。
+     */
+    @PreUpdate
+    protected void onUpdate() {
+        this.updated_At = new Date();
+    }
 }
