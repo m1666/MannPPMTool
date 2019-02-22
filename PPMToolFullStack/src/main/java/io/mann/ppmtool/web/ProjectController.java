a5e38921627b6f769594ed680bf87257240a03d5
Mao <160663534@qq.com>
2019-02-22 11:53:01 +0800

    The sixth course

--- PPMToolFullStack/src/main/java/io/mann/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/io/mann/ppmtool/web/ProjectController.java
@@ -1,17 +1,22 @@
 package io.mann.ppmtool.web;
 
 import io.mann.ppmtool.domain.Project;
+
 import io.mann.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Author: mann
@@ -28,7 +33,14 @@ public class ProjectController {
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
 
         if (result.hasErrors()) {
-            return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+
+            Map<String, String> errorMap = new HashMap<>();
+
+            for (FieldError error : result.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+
+            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
         }
 
         Project p = projectService.saveOrUpdateProject(project);
