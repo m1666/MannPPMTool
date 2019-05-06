36656e22393e0117cd4423acbcad3e47f8389a00
Mao <160663534@qq.com>
2019-05-07 00:07:58 +0800

    第二次Reactt课程

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
-      <Dashboard />
+      <Header />
       <Dashboard />
     </div>
   );
