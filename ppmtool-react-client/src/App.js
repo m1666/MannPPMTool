0aae9d7116ee87065322a8b342e7e8b3017cf2dc
Mao <160663534@qq.com>
2019-05-06 23:48:14 +0800

    第一次React课程

---
+++ ppmtool-react-client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+      <Dashboard />
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
