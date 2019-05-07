507b734f1182ab19043609d394f4d5117ab609da
Mao <160663534@qq.com>
2019-05-07 10:54:33 +0800

    First Functional component

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import ProjectItem from "./Project/ProjectItem";
+import CreateProjectButton from "./Project/CreateProjectButton";
 // import ProjectItem from "./Project/ProjectItem";
 
 class Dashboard extends Component {
@@ -11,9 +12,9 @@ class Dashboard extends Component {
             <div className="col-md-12">
               <h1 className="display-4 text-center">Projects</h1>
               <br />
-              <a href="ProjectForm.html" className="btn btn-lg btn-info">
-                Create a Project
-              </a>
+
+              <CreateProjectButton />
+
               <br />
               <hr />
               <ProjectItem />
