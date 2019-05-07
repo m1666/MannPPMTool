ffba857c81263638ba5018fa9ea3291fe3b64c6a
Mao <160663534@qq.com>
2019-05-07 09:35:17 +0800

    增加Bootstrap依赖

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
