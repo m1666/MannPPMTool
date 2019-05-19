33519ddac5145789a89d53f43cebb430974fc26c
“Mao” <160663534@qq.com>
2019-05-19 21:10:04 +0800

    Update project form and route

--- ppmtool-react-client/src/components/Project/AddProject.js
+++ ppmtool-react-client/src/components/Project/AddProject.js
@@ -64,7 +64,7 @@ class AddProject extends Component {
           <div className="container">
             <div className="row">
               <div className="col-md-8 m-auto">
-                <h5 className="display-4 text-center">Create Project form</h5>
+                <h5 className="display-4 text-center">Create Project</h5>
                 <hr />
                 <form onSubmit={this.onSubmit}>
                   <div className="form-group">
