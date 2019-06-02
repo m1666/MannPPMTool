bd29c1d80c1e5e85d76054af74127ad79d9025d4
“Mao” <160663534@qq.com>
2019-06-03 00:47:51 +0800

    Delete Project Task

--- ppmtool-react-client/src/actions/backlogActions.js
+++ ppmtool-react-client/src/actions/backlogActions.js
@@ -1,5 +1,10 @@
 import axios from "axios";
-import { GET_ERRORS, GET_BACKLOG, GET_PROJECT_TASK } from "./types";
+import {
+  GET_ERRORS,
+  GET_BACKLOG,
+  GET_PROJECT_TASK,
+  DELETE_PROJECT_TASK
+} from "./types";
 
 export const addProjectTask = (
   backlog_id,
@@ -69,6 +74,20 @@ export const updateProjectTask = (
     dispatch({
       type: GET_ERRORS,
       payload: err.response.data
+    });
+  }
+};
+
+export const deleteProjectTask = (backlog_id, pt_id) => async dispatch => {
+  if (
+    window.confirm(
+      `You are deleting project task ${pt_id}, this action cannot be undone`
+    )
+  ) {
+    await axios.delete(`/api/backlog/${backlog_id}/${pt_id}`);
+    dispatch({
+      type: DELETE_PROJECT_TASK,
+      payload: pt_id
     });
   }
 };
