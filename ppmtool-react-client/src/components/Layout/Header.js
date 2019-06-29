396149db6b7f5f1330488634157aa6f9f63b10eb
“Mao” <160663534@qq.com>
2019-06-30 00:44:23 +0800

    Intro to Securing the React App,Security Components

--- ppmtool-react-client/src/components/Layout/Header.js
+++ ppmtool-react-client/src/components/Layout/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
   render() {
@@ -6,9 +7,9 @@ class Header extends Component {
       // 导航栏
       <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
         <div className="container">
-          <a className="navbar-brand" href="Dashboard.html">
+          <Link className="navbar-brand" to="/">
             Personal Project Management Tool
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -21,9 +22,9 @@ class Header extends Component {
           <div className="collapse navbar-collapse" id="mobile-nav">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-                <a className="nav-link" href="/dashboard">
+                <Link className="nav-link" to="/dashboard">
                   Dashboard
-                </a>
+                </Link>
               </li>
             </ul>
 
